a3d7559a9a65ad8bebba1344e2956799de9ab888
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-04-27 00:01:29 +0200

    done with useState and conditional issue and hamburger menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,57 @@
-import React from 'react';
-
+// import React from 'react';
+import { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 const Navbar = () => {
-const routes = [
-    {
-        "id": 1,
-        "name": "Home",
-        "path": "/"
-    },
-    {
-        "id": 2,
-        "name": "About",
-        "path": "/about"
-    },
-    {
-        "id": 3,
-        "name": "Contact",
-        "path": "/contact"
-    },
-    {
-        "id": 4,
-        "name": "Products",
-        "path": "/products"
-    },
-    {
-        "id": 5,
-        "name": "Services",
-        "path": "/services"
-    }
-]
+  const [open, setOpen] = useState(false);
 
+  const routes = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "About",
+      path: "/about",
+    },
+    {
+      id: 3,
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      id: 4,
+      name: "Products",
+      path: "/products",
+    },
+    {
+      id: 5,
+      name: "Services",
+      path: "/services",
+    },
+  ];
 
+  return (
+    <div>
+         <span>{open === true ? "open" : "close"}</span>
+      <div>
+        <Bars3Icon
+          onClick={() => setOpen(!open)}
+          className="size-6 text-purple-700"
+        />
 
-    return (
-        <div>
-            {
-                routes.map(route => <li key={route.id}>{route.name}</li>)
-            }
-        </div>
-    );
+       
+      </div>
+
+      <ul className="md: flex">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
