a4e5c1b93b5aaf8170f0bf07212ab6a957c22301
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-04-27 15:27:45 +0200

    done with feature

--- src/Components/PriceCard/PriceCard.jsx
+++ src/Components/PriceCard/PriceCard.jsx
@@ -1,14 +1,20 @@
 // import React from 'react';
 
+import Feature from "../Features/Feature";
+
 const PriceCard = ({ price }) => {
   return (
-    <div>
-      <h4 >
+    <div className=" bg-indigo-400 mt-6 px-4 py-10 rounded">
+      <h4 className="text-center" >
         <span className="text-3xl font-extrabold text-purple-900 ">{price.price}</span>
         <span>/month</span>
         
       </h4>
-      <h2 className="text-4xl font-bold text-purple-500">{price.name}</h2>
+      <h2 className="text-4xl font-bold text-purple-500 text-center ">{price.name}</h2>
+      <p className=" font-bold text-2xl">Features : </p>
+      {
+        price.features.map((feature ,idx) => <Feature key={idx} feature={feature}></Feature>)
+      }
     </div>
   );
 };
--- src/Components/PriceList/PriceList.jsx
+++ src/Components/PriceList/PriceList.jsx
@@ -12,11 +12,11 @@ const PriceList = () => {
         .then(data => setPrices(data))
     }, [])
   return (
-    <div>
+    <div className="mx-12 ">
       <h1 className=" text-7xl mt-10 bg-purple-300 text-purple-800  text-center pt-4 pb-4">
         Awesome affordable prices
       </h1>
-      <div className=" grid grid-cols-3 gap-3">
+      <div className=" grid grid-cols-3 gap-3 ">
         {
             prices.map(price => <PriceCard key ={price.id} price={price}> </PriceCard>)
         }
